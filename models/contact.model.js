be346bcf633fe53de1394742e6ce0b59f4bb4521
Viyaan <viyaan.86@gmail.com>
2018-07-21 21:58:55 +0530

    contact list management backend

---
+++ models/contact.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+	_id:String,
+    name: String,
+    add: String,
+	tel: String,
+	email:String,
+	faceId:String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
